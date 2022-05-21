acf65e9c6ba37dd0ebfaf624f92fae5f58877fd8
John Xu <haoxuanxu@uchicago.edu>
2022-05-21 13:45:56 -0400

    add asset list

---
+++ config/assets/assets.go
@@ -0,0 +1,12 @@
+package assets
+
+var Asset = []string{
+	"IBM",
+	"AMZN",
+	"JNJ",
+	"TSLA",
+	"MSFT",
+	"AAPL",
+	"FB",
+	"AMC",
+}
--- strat/job.go
+++ strat/job.go
@@ -1,1 +1,5 @@
 package strat
+
+func MATradingStrategy(symbol string) {
+
+}
