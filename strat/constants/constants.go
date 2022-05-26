e5ece2bfd92dd2bb2f8011819e09bca8490dc87f
John Xu <haoxuanxu@uchicago.edu>
2022-05-26 11:04:49 -0400

    collapse support resistance calculation to one method

---
+++ strat/constants/constants.go
@@ -0,0 +1,8 @@
+package constants
+
+type Metric string
+
+const (
+	SUPPORT    Metric = "support"
+	RESISTANCE Metric = "resistance"
+)
--- strat/tools/getsupportresistance.go
+++ strat/tools/getsupportresistance.go
@@ -4,34 +4,30 @@ import (
 	"log"
 	"time"
 
+	"github.com/HaoxuanXu/MATradingBot/strat/constants"
 	"github.com/alpacahq/alpaca-trade-api-go/v2/marketdata"
 	"github.com/montanaflynn/stats"
 )
 
-func CalcSupport(barData []marketdata.Bar) float64 {
+func CalcSupportResistance(barData []marketdata.Bar, metric constants.Metric) float64 {
 	// we assume that the first value of the barData is the current value
+	var result float64
+	var err error
 	var barClosePriceData []float64
-	inputBarData := GetBarSubArray(barData[1:], time.Now(), 30)
+	inputBarData := GetBarSubArray(barData[1:], time.Now(), 20)
 	for _, bar := range inputBarData {
 		barClosePriceData = append(barClosePriceData, bar.Close)
 	}
-	support, err := stats.Min(barClosePriceData)
-	if err != nil {
-		log.Panicf("support level calculation failed: %v", err)
+	if metric == constants.SUPPORT {
+		result, err = stats.Min(barClosePriceData)
+		if err != nil {
+			log.Panicf("support level calculation failed: %v", err)
+		}
+	} else if metric == constants.RESISTANCE {
+		result, err = stats.Max(barClosePriceData)
+		if err != nil {
+			log.Panicf("resistance level calculation failed: %v", err)
+		}
 	}
-	return support
-}
-
-func CalcResistance(barData []marketdata.Bar) float64 {
-	// we assume that the first value of the barData is the current value
-	var barClosePriceData []float64
-	inputBarData := barData[1:]
-	for _, bar := range inputBarData {
-		barClosePriceData = append(barClosePriceData, bar.Close)
-	}
-	resistance, err := stats.Max(barClosePriceData)
-	if err != nil {
-		log.Panicf("resistance level calculation failed: %v", err)
-	}
-	return resistance
+	return result
 }
