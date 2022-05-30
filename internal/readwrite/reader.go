8ed3aff42def80f2482d65ba149d8020d1b5e6c9
John Xu <haoxuanxu@uchicago.edu>
2022-05-30 11:48:57 -0400

    debug

--- internal/readwrite/reader.go
+++ internal/readwrite/reader.go
@@ -3,7 +3,6 @@ package readwrite
 import (
 	"encoding/json"
 	"io/ioutil"
-	"log"
 
 	"github.com/HaoxuanXu/MATradingBot/db"
 )
@@ -12,15 +11,8 @@ func ReadFloatArrayToJson(symbol, side string) []float64 {
 	path := db.MapDataFilePath(side, symbol)
 	var resultContainer []float64
 
-	resultBytes, err := ioutil.ReadFile(path)
-	if err != nil {
-		log.Printf("file reading failed: %v\n", err)
-	}
+	resultBytes, _ := ioutil.ReadFile(path)
 
-	err = json.Unmarshal(resultBytes, &resultContainer)
-	if err != nil {
-		log.Printf("file unmarshalling failed: %v\n", err)
-	}
-
+	_ = json.Unmarshal(resultBytes, &resultContainer)
 	return resultContainer
 }
