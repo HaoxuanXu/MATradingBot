24bd6d5a557b61099b66f555931957d3d48c8f3f
John Xu <haoxuanxu@uchicago.edu>
2022-05-30 20:52:10 -0400

    change the way support resistance is calculated

--- strat/tools/getsupportresistance.go
+++ strat/tools/getsupportresistance.go
@@ -13,18 +13,20 @@ func CalcSupportResistance(barData []marketdata.Bar, metric constants.Metric) fl
 	// we assume that the first value of the barData is the current value
 	var result float64
 	var err error
-	var barClosePriceData []float64
+	var barHighPriceData []float64
+	var barLowPriceData []float64
 	inputBarData := GetBarSubArray(barData[1:], time.Now(), 20)
 	for _, bar := range inputBarData {
-		barClosePriceData = append(barClosePriceData, bar.Close)
+		barHighPriceData = append(barHighPriceData, bar.High)
+		barLowPriceData = append(barLowPriceData, bar.Low)
 	}
 	if metric == constants.SUPPORT {
-		result, err = stats.Min(barClosePriceData)
+		result, err = stats.Min(barLowPriceData)
 		if err != nil {
 			log.Fatalf("support level calculation failed: %v", err)
 		}
 	} else if metric == constants.RESISTANCE {
-		result, err = stats.Max(barClosePriceData)
+		result, err = stats.Max(barHighPriceData)
 		if err != nil {
 			log.Fatalf("resistance level calculation failed: %v", err)
 		}
