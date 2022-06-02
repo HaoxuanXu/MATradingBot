5689b557b86f39f907347f4e3fe9f8997d2acb90
John Xu <haoxuanxu@uchicago.edu>
2022-06-02 10:05:47 -0400

    modify

--- strat/dataprocessor/dataprocessor.go
+++ strat/dataprocessor/dataprocessor.go
@@ -63,10 +63,10 @@ func updateTrail(model *model.DataModel, data *model.TotalBarData) {
 	// 	currentBar.Low, model.Trails.LongTrailCandidate, model.Trails.ShortTrailCandidate, currentBar.Timestamp.String())
 
 	if len(model.Trails.LongTrailArray) > 0 {
-		model.Trails.AppliedLongTrail, _ = stats.Max(model.Trails.LongTrailArray)
+		model.Trails.AppliedLongTrail, _ = stats.Mean(model.Trails.LongTrailArray)
 	}
 	if len(model.Trails.ShortTrailArray) >= 0 {
-		model.Trails.AppliedShortTrail, _ = stats.Max(model.Trails.ShortTrailArray)
+		model.Trails.AppliedShortTrail, _ = stats.Mean(model.Trails.ShortTrailArray)
 	}
 
 }
