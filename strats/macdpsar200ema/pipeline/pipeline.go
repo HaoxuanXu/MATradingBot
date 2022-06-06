126d3805b91efc6b21f81feb867d01275de69a97
John Xu <haoxuanxu@uchicago.edu>
2022-06-06 09:34:12 -0400

    modify

--- strats/macdpsar200ema/pipeline/pipeline.go
+++ strats/macdpsar200ema/pipeline/pipeline.go
@@ -14,8 +14,8 @@ func RefreshPosition(model *model.DataModel, broker *api.AlpacaBroker) {
 
 func EnterBracketLongPosition(model *model.DataModel, data *model.TotalBarData, broker *api.AlpacaBroker, qty float64) {
 	currentTrade := data.TradeData[model.Symbol].Price
-	stop_loss := currentTrade - math.Min(math.Abs(currentTrade-model.Signal.CurrentParabolicSar), currentTrade*0.007)
-	take_proft := currentTrade + math.Min(math.Abs(currentTrade-model.Signal.CurrentParabolicSar), currentTrade*0.007)
+	stop_loss := currentTrade - math.Abs(currentTrade-model.Signal.CurrentParabolicSar)
+	take_proft := currentTrade + math.Abs(currentTrade-model.Signal.CurrentParabolicSar)
 	order := broker.SubmitBracketOrder(qty, take_proft, stop_loss, model.Symbol, "buy")
 	transaction.UpdatePositionAfterTransaction(model, order)
 	transaction.RecordEntryTransaction(model)
@@ -23,8 +23,8 @@ func EnterBracketLongPosition(model *model.DataModel, data *model.TotalBarData, 
 
 func EnterBracketShortPosition(model *model.DataModel, data *model.TotalBarData, broker *api.AlpacaBroker, qty float64) {
 	currentTrade := data.TradeData[model.Symbol].Price
-	stop_loss := currentTrade + math.Min(math.Abs(currentTrade-model.Signal.CurrentParabolicSar), currentTrade*0.007)
-	take_proft := currentTrade - math.Min(math.Abs(currentTrade-model.Signal.CurrentParabolicSar), currentTrade*0.007)
+	stop_loss := currentTrade + math.Abs(currentTrade-model.Signal.CurrentParabolicSar)
+	take_proft := currentTrade - math.Abs(currentTrade-model.Signal.CurrentParabolicSar)
 	order := broker.SubmitBracketOrder(qty, take_proft, stop_loss, model.Symbol, "sell")
 	transaction.UpdatePositionAfterTransaction(model, order)
 	transaction.RecordEntryTransaction(model)
