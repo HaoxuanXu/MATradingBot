d2bade8c877082bd35975f1b25adeb9c0d5090dd
John Xu <haoxuanxu@uchicago.edu>
2022-06-08 01:16:35 -0400

    use close data

--- strats/macdpsar200ema/signalcatcher/signalcatcher.go
+++ strats/macdpsar200ema/signalcatcher/signalcatcher.go
@@ -10,7 +10,7 @@ import (
 // In order to go long, 20MA has to be above  30MA and both MAs have to be rising
 func CanEnterLong(model *model.DataModel, broker *api.AlpacaBroker) bool {
 	if !model.Position.HasLongPosition && !model.Position.HasShortPosition &&
-		model.Signal.CurrentBar.Low > model.Signal.CurrentEMA200Period && // the current price is above the 200 period EMA value
+		model.Signal.CurrentBar.Close > model.Signal.CurrentEMA200Period && // the current price is above the 200 period EMA value
 		model.Signal.CurrentParabolicSar < model.Signal.CurrentBar.Low &&
 		model.Signal.PreviousParabolicSar > model.Signal.PreviousBar.High &&
 		time.Until(broker.Clock.NextClose) > time.Hour {
@@ -21,7 +21,7 @@ func CanEnterLong(model *model.DataModel, broker *api.AlpacaBroker) bool {
 
 func CanEnterShort(model *model.DataModel, broker *api.AlpacaBroker) bool {
 	if !model.Position.HasLongPosition && !model.Position.HasShortPosition &&
-		model.Signal.CurrentBar.High < model.Signal.CurrentEMA200Period && // the current price is below the 200 period EMA value
+		model.Signal.CurrentBar.Close < model.Signal.CurrentEMA200Period && // the current price is below the 200 period EMA value
 		model.Signal.CurrentParabolicSar > model.Signal.CurrentBar.High &&
 		model.Signal.PreviousParabolicSar < model.Signal.PreviousBar.Low &&
 		time.Until(broker.Clock.NextClose) > time.Hour {
