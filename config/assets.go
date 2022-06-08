69d8f5ccc8ad9af29edd73e2354d8529fc26ccc3
John Xu <haoxuanxu@uchicago.edu>
2022-06-08 13:34:26 -0400

    update asset

--- config/assets.go
+++ config/assets.go
@@ -1,15 +1,7 @@
 package config
 
-var Assets = []string{
-	"IBM",
-	"AMZN",
-	"JNJ",
-	"TSLA",
-	"MSFT",
-	"AAPL",
-	"FB",
-	"AMC",
-	"DIS",
-	"GOOG",
-	"NFLX",
-}
+var Assets = []string{"TSLA", "AAPL", "AMZN", "AMC", "F", "SNDL", "MSFT", "DIS", "NIO", "FB", "TWTR", "NFLX", "LCID", "NVDA",
+	"PFE", "SNAP", "VOO", "GPRO", "PLUG", "AAL", "BABA", "SPY", "HOOD", "ACB", "CCL", "BAC", "PLTR", "NOK", "SBUX", "DAL", "GOOGL",
+	"AMD", "GME", "RIVN", "COIN", "TLRY", "OGI", "KO", "T", "CGC", "SPCE", "PYPL", "VTI", "MRNA", "UBER", "BB", "RBLX", "GM", "FCEL",
+	"CENN", "GE", "SQ", "WKHS", "IDEX", "BA", "DKNG", "WMT", "ABNB", "UAL", "QQQ", "JNJ", "XOM", "ARKK", "NKE", "SOFI", "PTON", "RIOT",
+	"SONY", "INTC", "BRK.B", "JPM", "ZM"}
