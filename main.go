329f830d7c5b5fb0d7a3eb42e29fa3f1c1979cfb
John Xu <haoxuanxu@uchicago.edu>
2022-06-08 10:09:03 -0400

    modify

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"time"
 
+	"github.com/HaoxuanXu/MATradingBot/internal/api"
 	"github.com/go-co-op/gocron"
 )
 
@@ -12,6 +13,10 @@ func main() {
 	s := gocron.NewScheduler(loc)
 	s.Cron("30 9 * * 1-5").Do(Run)
 	log.Println("Waiting to run the job")
+
+	if api.GetBroker().Clock.IsOpen {
+		Run()
+	}
 
 	s.StartBlocking()
 }
