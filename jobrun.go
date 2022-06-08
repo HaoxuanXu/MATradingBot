76dae5e796cb05d6aaeaada016c514d4593a5b2b
John Xu <haoxuanxu@uchicago.edu>
2022-06-08 00:29:53 -0400

    add scheduler

---
+++ jobrun.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"strconv"
+	"time"
+
+	"github.com/HaoxuanXu/MATradingBot/config"
+	"github.com/HaoxuanXu/MATradingBot/db"
+	"github.com/HaoxuanXu/MATradingBot/internal/api"
+	"github.com/HaoxuanXu/MATradingBot/internal/channel"
+	"github.com/HaoxuanXu/MATradingBot/internal/logging"
+	"github.com/HaoxuanXu/MATradingBot/strats/macdpsar200ema"
+	"github.com/HaoxuanXu/MATradingBot/strats/macdpsar200ema/model"
+	"github.com/HaoxuanXu/MATradingBot/util"
+)
+
+func Run() {
+	// read from flag
+	yamlFileName := flag.String("config", "production-paper-account.yml", "this yml config file for the application")
+	flag.Parse()
+
+	yamlConfig := util.ReadYAMLFile(db.MapYAMLConfigPath(*yamlFileName))
+
+	var totalData model.TotalBarData
+	assets := config.Assets
+
+	accountType := fmt.Sprintf("%s", yamlConfig["accounttype"])
+	serverType := fmt.Sprintf("%s", yamlConfig["servertype"])
+	totalEntryPercent, _ := strconv.ParseFloat(fmt.Sprintf("%v", yamlConfig["entrypercent"]), 64)
+	workerEntryPercent := totalEntryPercent / float64(len(assets))
+
+	// set up logging
+	logFile := logging.SetLogging()
+
+	dataEngine := api.GetDataEngine(accountType, serverType)
+	broker := api.GetBroker(accountType, serverType)
+
+	// create channel map
+	chanMap := channel.CreateMap(assets)
+
+	if !broker.Clock.IsOpen {
+		log.Printf("Wait for %.2f minutes till the market opens\n", time.Until(broker.Clock.NextOpen).Minutes())
+		time.Sleep(time.Until(broker.Clock.NextOpen))
+	}
+
+	// start workers
+	for _, asset := range assets {
+		log.Printf("Starting worker for %s trading\n", asset)
+		go macdpsar200ema.MACDPSar200EMAStrategy(asset, accountType, serverType, workerEntryPercent, &totalData, chanMap.Map[asset])
+	}
+
+	// start main loop
+	log.Println("Start main loop...")
+	broker.Clock, _ = broker.Client.GetClock()
+	for time.Until(broker.Clock.NextClose) > 0 {
+		barData := dataEngine.GetMultiBars(5, assets)
+		if len(barData) > 0 {
+			totalData.BarData = barData
+		}
+		totalData.QuoteData = dataEngine.GetLatestMultiQuotes(assets)
+		chanMap.TriggerWorkers()
+		time.Sleep(time.Minute)
+	}
+	// close operation when the market is closed
+	log.Println("Shutting down workers...")
+	chanMap.CloseWorkers()
+	log.Println("Closing channels...")
+	chanMap.CloseChannels()
+	logFile.Close()
+}
