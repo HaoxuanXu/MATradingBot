526c35dc8ac8f8ccf3881bbf3d3aa2d1550cac2e
John Xu <haoxuanxu@uchicago.edu>
2022-06-08 10:10:59 -0400

    debug

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"time"
 
+	"github.com/HaoxuanXu/MATradingBot/db"
 	"github.com/HaoxuanXu/MATradingBot/internal/api"
+	"github.com/HaoxuanXu/MATradingBot/util"
 	"github.com/go-co-op/gocron"
 )
 
@@ -14,7 +18,14 @@ func main() {
 	s.Cron("30 9 * * 1-5").Do(Run)
 	log.Println("Waiting to run the job")
 
-	if api.GetBroker().Clock.IsOpen {
+	yamlFileName := flag.String("config", "production-paper-account.yml", "this yml config file for the application")
+	flag.Parse()
+
+	yamlConfig := util.ReadYAMLFile(db.MapYAMLConfigPath(*yamlFileName))
+	accountType := fmt.Sprintf("%s", yamlConfig["accounttype"])
+	serverType := fmt.Sprintf("%s", yamlConfig["servertype"])
+
+	if api.GetBroker(accountType, serverType).Clock.IsOpen {
 		Run()
 	}
 
