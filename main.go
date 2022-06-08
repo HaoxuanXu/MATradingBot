3194490b426906a37fa8e95466c5912898e3af0e
John Xu <haoxuanxu@uchicago.edu>
2022-06-08 08:53:03 -0400

    modify

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	loc, _ := time.LoadLocation("America/New_York")
 	s := gocron.NewScheduler(loc)
-	s.Cron("30 8 * * 1-5").Do(Run)
+	s.Cron("30 9 * * 1-5").Do(Run)
 	log.Println("Waiting to run the job")
 
 	s.StartBlocking()
