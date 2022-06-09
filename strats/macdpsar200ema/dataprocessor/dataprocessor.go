d54ea606cb19c71ec72fe810fbbbd882a1e7d197
John Xu <haoxuanxu@uchicago.edu>
2022-06-08 22:34:02 -0400

    debug

--- strats/macdpsar200ema/dataprocessor/dataprocessor.go
+++ strats/macdpsar200ema/dataprocessor/dataprocessor.go
@@ -49,30 +49,33 @@ func ProcessBarData(model *model.DataModel, data *model.TotalBarData, isCrypto b
 	} else {
 		// there is an update in the data, we then proceed the following processes
 		// retrieve close data from bar slice
-		var closeBars []float64
-		var highBars []float64
-		var lowBars []float64
-		for _, bar := range data.CryptoBarData[model.Symbol] {
-			closeBars = append(closeBars, bar.Close)
-			highBars = append(highBars, bar.High)
-			lowBars = append(lowBars, bar.Low)
+		if model.Signal.CurrentCryptoBar.Timestamp != model.CurrentBarTimestamp {
+			var closeBars []float64
+			var highBars []float64
+			var lowBars []float64
+			for _, bar := range data.CryptoBarData[model.Symbol] {
+				closeBars = append(closeBars, bar.Close)
+				highBars = append(highBars, bar.High)
+				lowBars = append(lowBars, bar.Low)
+			}
+
+			// calculate the current 200 period EMA value
+			ema200Period := indicator.Ema(200, closeBars)
+			model.Signal.CurrentEMA200Period = ema200Period[len(ema200Period)-1]
+
+			// calculate the current parabolic sar
+			parSarVals, _ := indicator.ParabolicSar(highBars, lowBars, closeBars)
+			model.Signal.CurrentParabolicSar = parSarVals[len(parSarVals)-1]
+			model.Signal.PreviousParabolicSar = parSarVals[len(parSarVals)-2]
+			// calculate the current MACD values (MACD line, MACD signal line)
+			macd, macdSignal := indicator.Macd(closeBars)
+			model.Signal.CurrentMacd = macd[len(macd)-1]
+			model.Signal.CurrentMacdSignal = macdSignal[len(macdSignal)-1]
+
+			model.CurrentBarTimestamp = model.Signal.CurrentCryptoBar.Timestamp
+			return true
 		}
 
-		// calculate the current 200 period EMA value
-		ema200Period := indicator.Ema(200, closeBars)
-		model.Signal.CurrentEMA200Period = ema200Period[len(ema200Period)-1]
-
-		// calculate the current parabolic sar
-		parSarVals, _ := indicator.ParabolicSar(highBars, lowBars, closeBars)
-		model.Signal.CurrentParabolicSar = parSarVals[len(parSarVals)-1]
-		model.Signal.PreviousParabolicSar = parSarVals[len(parSarVals)-2]
-		// calculate the current MACD values (MACD line, MACD signal line)
-		macd, macdSignal := indicator.Macd(closeBars)
-		model.Signal.CurrentMacd = macd[len(macd)-1]
-		model.Signal.CurrentMacdSignal = macdSignal[len(macdSignal)-1]
-
-		model.CurrentBarTimestamp = model.Signal.CurrentCryptoBar.Timestamp
-		return true
 	}
 
 	return false
