a364b545a294237015884a7f76f8f859c6a8247d
John Xu <haoxuanxu@uchicago.edu>
2022-06-09 15:58:04 -0400

    reduce stock asset list

--- config/assets.go
+++ config/assets.go
@@ -1,7 +1,7 @@
 package config
 
 var Stocks = []string{"TSLA", "AAPL", "AMZN", "AMC", "F", "SNDL", "MSFT", "DIS", "NIO", "FB", "TWTR", "NFLX", "NVDA",
-	"PFE", "SNAP", "GPRO", "PLUG", "AAL", "BABA", "SPY", "HOOD", "ACB", "CCL", "BAC", "PLTR", "NOK", "SBUX", "DAL", "GOOGL",
+	"PFE", "SNAP", "GPRO", "PLUG", "AAL", "BABA", "HOOD", "ACB", "CCL", "BAC", "PLTR", "NOK", "SBUX", "DAL", "GOOGL",
 	"AMD", "GME", "RIVN", "COIN", "TLRY", "OGI", "KO", "T", "CGC", "SPCE", "PYPL", "VTI", "MRNA", "UBER", "BB", "RBLX", "GM", "FCEL",
-	"CENN", "GE", "SQ", "WKHS", "IDEX", "BA", "DKNG", "WMT", "ABNB", "UAL", "QQQ", "JNJ", "XOM", "ARKK", "NKE", "SOFI", "PTON", "RIOT",
+	"CENN", "GE", "SQ", "WKHS", "IDEX", "BA", "DKNG", "WMT", "ABNB", "UAL", "JNJ", "XOM", "ARKK", "NKE", "SOFI", "PTON", "RIOT",
 	"SONY", "INTC", "BRK.B", "JPM", "ZM", "TGT"}
