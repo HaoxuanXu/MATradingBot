ffbf6491c247cbeff9ea8a83cb5dd69362fa2816
John Xu <haoxuanxu@uchicago.edu>
2022-06-13 14:48:54 -0400

    remove low price assets

--- config/assets.go
+++ config/assets.go
@@ -1,6 +1,6 @@
 package config
 
-var Stocks = []string{"TSLA", "AAPL", "AMZN", "F", "SNDL", "MSFT", "DIS", "NIO", "TWTR", "NFLX", "NVDA",
-	"PFE", "SNAP", "GPRO", "PLUG", "AAL", "BABA", "ACB", "BAC", "PLTR", "NOK", "SBUX", "DAL", "GOOGL",
-	"AMD", "COIN", "OGI", "KO", "T", "SPCE", "PYPL", "VTI", "MRNA", "UBER", "BB", "RBLX", "GM", "GE", "SQ", "WKHS", "IDEX", "BA", "DKNG", "WMT", "ABNB", "UAL", "JNJ", "XOM", "ARKK", "NKE", "SOFI", "PTON", "RIOT",
+var Stocks = []string{"TSLA", "AAPL", "AMZN", "F", "MSFT", "DIS", "NIO", "TWTR", "NFLX", "NVDA",
+	"PFE", "SNAP", "GPRO", "AAL", "BABA", "ACB", "BAC", "PLTR", "SBUX", "DAL", "GOOGL",
+	"AMD", "COIN", "OGI", "KO", "T", "SPCE", "PYPL", "VTI", "MRNA", "UBER", "BB", "RBLX", "GM", "GE", "SQ", "IDEX", "BA", "DKNG", "WMT", "ABNB", "UAL", "JNJ", "XOM", "ARKK", "NKE", "SOFI", "PTON", "RIOT",
 	"SONY", "INTC", "BRK.B", "JPM", "ZM", "TGT"}
