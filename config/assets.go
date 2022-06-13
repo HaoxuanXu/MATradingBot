01596b6623a6c659d0bcee1e613250d4f50eb693
John Xu <haoxuanxu@uchicago.edu>
2022-06-13 11:14:24 -0400

    modify

--- config/assets.go
+++ config/assets.go
@@ -1,6 +1,6 @@
 package config
 
-var Stocks = []string{"TSLA", "AAPL", "AMZN", "AMC", "F", "SNDL", "MSFT", "DIS", "NIO", "META", "TWTR", "NFLX", "NVDA",
-	"PFE", "SNAP", "GPRO", "PLUG", "AAL", "BABA", "ACB", "CCL", "BAC", "PLTR", "NOK", "SBUX", "DAL", "GOOGL",
+var Stocks = []string{"TSLA", "AAPL", "AMZN", "AMC", "F", "SNDL", "MSFT", "DIS", "NIO", "TWTR", "NFLX", "NVDA",
+	"PFE", "SNAP", "GPRO", "PLUG", "AAL", "BABA", "ACB", "BAC", "PLTR", "NOK", "SBUX", "DAL", "GOOGL",
 	"AMD", "COIN", "OGI", "KO", "T", "SPCE", "PYPL", "VTI", "MRNA", "UBER", "BB", "RBLX", "GM", "FCEL", "GE", "SQ", "WKHS", "IDEX", "BA", "DKNG", "WMT", "ABNB", "UAL", "JNJ", "XOM", "ARKK", "NKE", "SOFI", "PTON", "RIOT",
 	"SONY", "INTC", "BRK.B", "JPM", "ZM", "TGT"}
