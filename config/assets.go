6c2356dada55817ad5494c626adbdf4914cb38a4
John Xu <haoxuanxu@uchicago.edu>
2022-06-13 11:21:18 -0400

    modify

--- config/assets.go
+++ config/assets.go
@@ -1,6 +1,6 @@
 package config
 
-var Stocks = []string{"TSLA", "AAPL", "AMZN", "AMC", "F", "SNDL", "MSFT", "DIS", "NIO", "TWTR", "NFLX", "NVDA",
+var Stocks = []string{"TSLA", "AAPL", "AMZN", "F", "SNDL", "MSFT", "DIS", "NIO", "TWTR", "NFLX", "NVDA",
 	"PFE", "SNAP", "GPRO", "PLUG", "AAL", "BABA", "ACB", "BAC", "PLTR", "NOK", "SBUX", "DAL", "GOOGL",
-	"AMD", "COIN", "OGI", "KO", "T", "SPCE", "PYPL", "VTI", "MRNA", "UBER", "BB", "RBLX", "GM", "FCEL", "GE", "SQ", "WKHS", "IDEX", "BA", "DKNG", "WMT", "ABNB", "UAL", "JNJ", "XOM", "ARKK", "NKE", "SOFI", "PTON", "RIOT",
+	"AMD", "COIN", "OGI", "KO", "T", "SPCE", "PYPL", "VTI", "MRNA", "UBER", "BB", "RBLX", "GM", "GE", "SQ", "WKHS", "IDEX", "BA", "DKNG", "WMT", "ABNB", "UAL", "JNJ", "XOM", "ARKK", "NKE", "SOFI", "PTON", "RIOT",
 	"SONY", "INTC", "BRK.B", "JPM", "ZM", "TGT"}
--- strats/macdpsar200ema/signalcatcher/signalcatcher.go
+++ strats/macdpsar200ema/signalcatcher/signalcatcher.go
@@ -52,7 +52,7 @@ func CanEnterLong(model *model.DataModel, broker *api.AlpacaBroker) bool {
 	if !model.Position.HasLongPosition && !model.Position.HasShortPosition &&
 		model.Signal.Bars[len(model.Signal.Bars)-1].Close > model.Signal.EMA200Periods[len(model.Signal.EMA200Periods)-1] && // the current price is above the 200 period EMA value
 		model.Signal.ParabolicSars[len(model.Signal.ParabolicSars)-1] < model.Signal.Bars[len(model.Signal.Bars)-1].Low &&
-		pSarPrevDirection(model.Signal.ParabolicSars[len(model.Signal.ParabolicSars)-3:], model.Signal.Bars[len(model.Signal.Bars)-3:], "below") &&
+		pSarPrevDirection(model.Signal.ParabolicSars[len(model.Signal.ParabolicSars)-2:], model.Signal.Bars[len(model.Signal.Bars)-2:], "below") &&
 		model.Signal.Macds[len(model.Signal.Macds)-1] > model.Signal.MacdSignals[len(model.Signal.MacdSignals)-1] {
 		return true
 	}
@@ -64,7 +64,7 @@ func CanEnterShort(model *model.DataModel, broker *api.AlpacaBroker) bool {
 	if !model.Position.HasLongPosition && !model.Position.HasShortPosition &&
 		model.Signal.Bars[len(model.Signal.Bars)-1].Close < model.Signal.EMA200Periods[len(model.Signal.EMA200Periods)-1] && // the current price is below the 200 period EMA value
 		model.Signal.ParabolicSars[len(model.Signal.ParabolicSars)-1] > model.Signal.Bars[len(model.Signal.Bars)-1].High &&
-		pSarPrevDirection(model.Signal.ParabolicSars[len(model.Signal.ParabolicSars)-3:], model.Signal.Bars[len(model.Signal.Bars)-3:], "above") &&
+		pSarPrevDirection(model.Signal.ParabolicSars[len(model.Signal.ParabolicSars)-2:], model.Signal.Bars[len(model.Signal.Bars)-2:], "above") &&
 		model.Signal.Macds[len(model.Signal.Macds)-1] < model.Signal.MacdSignals[len(model.Signal.MacdSignals)-1] {
 		return true
 	}
