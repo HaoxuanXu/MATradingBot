52c94bb4fe2278a86f8639af5d20372c14d2683a
John Xu <haoxuanxu@uchicago.edu>
2022-06-13 11:12:05 -0400

    remove non-shortable assets

--- config/assets.go
+++ config/assets.go
@@ -1,7 +1,6 @@
 package config
 
 var Stocks = []string{"TSLA", "AAPL", "AMZN", "AMC", "F", "SNDL", "MSFT", "DIS", "NIO", "META", "TWTR", "NFLX", "NVDA",
-	"PFE", "SNAP", "GPRO", "PLUG", "AAL", "BABA", "HOOD", "ACB", "CCL", "BAC", "PLTR", "NOK", "SBUX", "DAL", "GOOGL",
-	"AMD", "GME", "RIVN", "COIN", "TLRY", "OGI", "KO", "T", "CGC", "SPCE", "PYPL", "VTI", "MRNA", "UBER", "BB", "RBLX", "GM", "FCEL",
-	"CENN", "GE", "SQ", "WKHS", "IDEX", "BA", "DKNG", "WMT", "ABNB", "UAL", "JNJ", "XOM", "ARKK", "NKE", "SOFI", "PTON", "RIOT",
+	"PFE", "SNAP", "GPRO", "PLUG", "AAL", "BABA", "ACB", "CCL", "BAC", "PLTR", "NOK", "SBUX", "DAL", "GOOGL",
+	"AMD", "COIN", "OGI", "KO", "T", "SPCE", "PYPL", "VTI", "MRNA", "UBER", "BB", "RBLX", "GM", "FCEL", "GE", "SQ", "WKHS", "IDEX", "BA", "DKNG", "WMT", "ABNB", "UAL", "JNJ", "XOM", "ARKK", "NKE", "SOFI", "PTON", "RIOT",
 	"SONY", "INTC", "BRK.B", "JPM", "ZM", "TGT"}
