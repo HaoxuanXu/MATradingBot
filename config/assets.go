7d035c168156f61f929decace7cf54af1970aade
John Xu <haoxuanxu@uchicago.edu>
2022-06-13 11:08:24 -0400

    change facebook symbol

--- config/assets.go
+++ config/assets.go
@@ -1,6 +1,6 @@
 package config
 
-var Stocks = []string{"TSLA", "AAPL", "AMZN", "AMC", "F", "SNDL", "MSFT", "DIS", "NIO", "FB", "TWTR", "NFLX", "NVDA",
+var Stocks = []string{"TSLA", "AAPL", "AMZN", "AMC", "F", "SNDL", "MSFT", "DIS", "NIO", "META", "TWTR", "NFLX", "NVDA",
 	"PFE", "SNAP", "GPRO", "PLUG", "AAL", "BABA", "HOOD", "ACB", "CCL", "BAC", "PLTR", "NOK", "SBUX", "DAL", "GOOGL",
 	"AMD", "GME", "RIVN", "COIN", "TLRY", "OGI", "KO", "T", "CGC", "SPCE", "PYPL", "VTI", "MRNA", "UBER", "BB", "RBLX", "GM", "FCEL",
 	"CENN", "GE", "SQ", "WKHS", "IDEX", "BA", "DKNG", "WMT", "ABNB", "UAL", "JNJ", "XOM", "ARKK", "NKE", "SOFI", "PTON", "RIOT",
