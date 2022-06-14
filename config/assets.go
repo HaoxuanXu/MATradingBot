de4191fe43baa3919e0d65e501511a268cd5666b
John Xu <haoxuanxu@uchicago.edu>
2022-06-14 15:40:29 -0400

    remove aurora

--- config/assets.go
+++ config/assets.go
@@ -1,6 +1,6 @@
 package config
 
 var Stocks = []string{"TSLA", "AAPL", "AMZN", "F", "MSFT", "DIS", "NIO", "TWTR", "NFLX", "NVDA",
-	"PFE", "SNAP", "GPRO", "AAL", "BABA", "ACB", "BAC", "PLTR", "SBUX", "DAL", "GOOGL",
+	"PFE", "SNAP", "GPRO", "AAL", "BABA", "BAC", "PLTR", "SBUX", "DAL", "GOOGL",
 	"AMD", "COIN", "OGI", "KO", "T", "SPCE", "PYPL", "VTI", "MRNA", "UBER", "BB", "RBLX", "GM", "GE", "SQ", "BA", "DKNG", "WMT", "ABNB", "UAL", "JNJ", "XOM", "ARKK", "NKE", "SOFI", "PTON",
 	"SONY", "INTC", "BRK.B", "JPM", "ZM", "TGT"}
