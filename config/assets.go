fa27dcf610206992191d65f44208782154ac1310
John Xu <haoxuanxu@uchicago.edu>
2022-06-15 14:10:59 -0400

    remove OGI

--- config/assets.go
+++ config/assets.go
@@ -2,5 +2,5 @@ package config
 
 var Stocks = []string{"TSLA", "AAPL", "AMZN", "F", "MSFT", "DIS", "NIO", "TWTR", "NFLX", "NVDA",
 	"PFE", "SNAP", "GPRO", "AAL", "BABA", "BAC", "PLTR", "SBUX", "DAL", "GOOGL",
-	"AMD", "COIN", "OGI", "KO", "T", "SPCE", "PYPL", "VTI", "MRNA", "UBER", "BB", "RBLX", "GM", "GE", "SQ", "BA", "DKNG", "WMT", "ABNB", "UAL", "JNJ", "XOM", "ARKK", "NKE", "SOFI", "PTON",
+	"AMD", "COIN", "KO", "T", "SPCE", "PYPL", "VTI", "MRNA", "UBER", "BB", "RBLX", "GM", "GE", "SQ", "BA", "DKNG", "WMT", "ABNB", "UAL", "JNJ", "XOM", "ARKK", "NKE", "SOFI", "PTON",
 	"SONY", "INTC", "BRK.B", "JPM", "ZM", "TGT"}
