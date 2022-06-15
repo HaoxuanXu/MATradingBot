94c50b08e1cc9e4a47d38e703612b63183f378b2
John Xu <haoxuanxu@uchicago.edu>
2022-06-15 10:41:29 -0400

    modify:

--- strats/macdpsar200ema/signalcatcher/signalcatcher.go
+++ strats/macdpsar200ema/signalcatcher/signalcatcher.go
@@ -10,8 +10,6 @@ func CanEnterLong(model *model.DataModel, broker *api.AlpacaBroker) bool {
 	if !model.Position.HasLongPosition && !model.Position.HasShortPosition &&
 		model.Signal.Bars[len(model.Signal.Bars)-1].Close > model.Signal.EMA200Periods[len(model.Signal.EMA200Periods)-1] && // the current price is above the 200 period EMA value
 		model.Signal.Macds[len(model.Signal.Macds)-1] > model.Signal.MacdSignals[len(model.Signal.MacdSignals)-1] &&
-		model.Signal.Macds[len(model.Signal.Macds)-1] < 0 &&
-		model.Signal.MacdSignals[len(model.Signal.MacdSignals)-1] < 0 &&
 		model.Signal.StochOversold &&
 		!model.Signal.StockOverbought &&
 		model.Signal.RSI[len(model.Signal.RSI)-1] > 50 {
@@ -25,8 +23,6 @@ func CanEnterShort(model *model.DataModel, broker *api.AlpacaBroker) bool {
 	if !model.Position.HasLongPosition && !model.Position.HasShortPosition &&
 		model.Signal.Bars[len(model.Signal.Bars)-1].Close < model.Signal.EMA200Periods[len(model.Signal.EMA200Periods)-1] && // the current price is below the 200 period EMA value
 		model.Signal.Macds[len(model.Signal.Macds)-1] < model.Signal.MacdSignals[len(model.Signal.MacdSignals)-1] &&
-		model.Signal.Macds[len(model.Signal.Macds)-1] > 0 &&
-		model.Signal.MacdSignals[len(model.Signal.MacdSignals)-1] > 0 &&
 		!model.Signal.StochOversold &&
 		model.Signal.StockOverbought &&
 		model.Signal.RSI[len(model.Signal.RSI)-1] < 50 {
