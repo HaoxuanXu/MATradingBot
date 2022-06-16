552ee2d4b41a5703a2af2236a4972fc9b7641cb3
John Xu <haoxuanxu@uchicago.edu>
2022-06-16 10:15:45 -0400

    modify

--- strats/macdpsar200ema/signalcatcher/signalcatcher.go
+++ strats/macdpsar200ema/signalcatcher/signalcatcher.go
@@ -6,9 +6,8 @@ import (
 
 // In order to go long, 20MA has to be above  30MA and both MAs have to be rising
 func CanEnterLongFastPeriod(model *model.DataModel) bool {
-	if model.Signal.Macds[len(model.Signal.Macds)-1] < model.Signal.MacdSignals[len(model.Signal.MacdSignals)-1] &&
-		model.Signal.Macds[len(model.Signal.Macds)-1]-model.Signal.MacdSignals[len(model.Signal.MacdSignals)-1] >
-			model.Signal.Macds[len(model.Signal.Macds)-2]-model.Signal.MacdSignals[len(model.Signal.MacdSignals)-2] &&
+	if model.Signal.Macds[len(model.Signal.Macds)-1]-model.Signal.MacdSignals[len(model.Signal.MacdSignals)-1] >
+		model.Signal.Macds[len(model.Signal.Macds)-2]-model.Signal.MacdSignals[len(model.Signal.MacdSignals)-2] &&
 		!model.Signal.StochOverbought && model.Signal.StochOversold &&
 		model.Signal.StochK[len(model.Signal.StochK)-1] > model.Signal.StochD[len(model.Signal.StochD)-1] {
 		return true
@@ -28,9 +27,8 @@ func CanEnterLongSlowPeriod(model *model.DataModel) bool {
 }
 
 func CanEnterShortFastPeriod(model *model.DataModel) bool {
-	if model.Signal.Macds[len(model.Signal.Macds)-1] > model.Signal.MacdSignals[len(model.Signal.MacdSignals)-1] &&
-		model.Signal.Macds[len(model.Signal.Macds)-1]-model.Signal.MacdSignals[len(model.Signal.MacdSignals)-1] <
-			model.Signal.Macds[len(model.Signal.Macds)-2]-model.Signal.MacdSignals[len(model.Signal.MacdSignals)-2] &&
+	if model.Signal.Macds[len(model.Signal.Macds)-1]-model.Signal.MacdSignals[len(model.Signal.MacdSignals)-1] <
+		model.Signal.Macds[len(model.Signal.Macds)-2]-model.Signal.MacdSignals[len(model.Signal.MacdSignals)-2] &&
 		!model.Signal.StochOversold && model.Signal.StochOverbought &&
 		model.Signal.StochK[len(model.Signal.StochK)-1] < model.Signal.StochD[len(model.Signal.StochD)-1] {
 		return true
