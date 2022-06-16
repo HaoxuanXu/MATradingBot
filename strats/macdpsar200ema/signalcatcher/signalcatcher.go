f10b159e5863d306d9f10b2680a0fd8171e83e1b
John Xu <haoxuanxu@uchicago.edu>
2022-06-16 10:31:43 -0400

    modify

--- strats/macdpsar200ema/signalcatcher/signalcatcher.go
+++ strats/macdpsar200ema/signalcatcher/signalcatcher.go
@@ -8,7 +8,7 @@ import (
 func CanEnterLongFastPeriod(model *model.DataModel) bool {
 	if model.Signal.Macds[len(model.Signal.Macds)-1]-model.Signal.MacdSignals[len(model.Signal.MacdSignals)-1] >
 		model.Signal.Macds[len(model.Signal.Macds)-2]-model.Signal.MacdSignals[len(model.Signal.MacdSignals)-2] &&
-		model.Signal.StochK[len(model.Signal.StochK)-1] > model.Signal.StochD[len(model.Signal.StochD)-1] {
+		!model.Signal.StochOverbought && model.Signal.StochOversold {
 		return true
 	}
 
@@ -29,7 +29,7 @@ func CanEnterLongSlowPeriod(model *model.DataModel) bool {
 func CanEnterShortFastPeriod(model *model.DataModel) bool {
 	if model.Signal.Macds[len(model.Signal.Macds)-1]-model.Signal.MacdSignals[len(model.Signal.MacdSignals)-1] <
 		model.Signal.Macds[len(model.Signal.Macds)-2]-model.Signal.MacdSignals[len(model.Signal.MacdSignals)-2] &&
-		model.Signal.StochK[len(model.Signal.StochK)-1] < model.Signal.StochD[len(model.Signal.StochD)-1] {
+		!model.Signal.StochOversold && model.Signal.StochOverbought {
 		return true
 	}
 	return false
